b56ba7ffd0c461f55007314e16f3fff0658ef409
F1renze <f1renze@163.com>
2019-12-28 23:30:07 +0800

    update: 2 pointers * 1

---
+++ solutions/q881/boats.go
@@ -0,0 +1,24 @@
+package q881
+
+import "sort"
+
+func numRescueBoats(people []int, limit int) int {
+	if len(people) < 1 {
+		return 0
+	}
+	sort.Ints(people)
+
+	s, e := 0, len(people)-1
+	c := 0
+
+	for s <= e {
+		if people[s] + people[e] <= limit {
+			s++
+			e--
+		} else if people[e] <= limit {
+			e--
+		}
+		c++
+	}
+	return c
+}
