93e03bc940a694247b083aa191973c167655909c
F1renze <f1renze@163.com>
2020-01-15 23:16:42 +0800

    update: bin search * 1

---
+++ solutions/q240/matrix.go
@@ -0,0 +1,19 @@
+package q240
+
+
+func searchMatrix(matrix [][]int, target int) bool {
+	if len(matrix) < 1 || len(matrix[0]) < 1 {
+		return false
+	}
+	i, j := len(matrix) - 1, 0
+	for i >= 0 && j < len(matrix[0]) {
+		if matrix[i][j] < target {
+			j++
+		} else if matrix[i][j] > target {
+			i--
+		} else {
+			return true
+		}
+	}
+	return false
+}
