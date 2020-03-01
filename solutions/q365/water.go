57d1e34b09facd3019dce7bc0dfe01e2872c9e58
F1renze <f1renze@163.com>
2020-03-02 00:19:37 +0800

    update: math * 1

---
+++ solutions/q365/water.go
@@ -0,0 +1,19 @@
+package q365
+
+
+func canMeasureWater(x int, y int, z int) bool {
+	if x + y < z {
+		return false
+	}
+	if x == 0 || y == 0 {
+		return z == 0 || x + y == z
+	}
+	return (z % gcd(x, y)) == 0
+}
+
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return gcd(b, a % b)
+}
